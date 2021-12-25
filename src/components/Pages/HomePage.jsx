b1d0a044ae91fff51bce93a29ce5b42805b0ccfb
Claude Ni <49127719+ClaudeNi@users.noreply.github.com>
2021-12-25 22:44:09 +0200

    Add files via upload

--- src/components/Pages/HomePage.jsx
+++ src/components/Pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class HomePage extends React.Component {
-
     componentDidMount() {
         const myStorage = window.localStorage;
         if (myStorage.cart === undefined) {
@@ -10,8 +10,18 @@ class HomePage extends React.Component {
     }
 
     render() {
-    return <div>Welcome to Ni Store</div>
+        return (
+            <div className="homepage">
+                <span className="title">Welcome to Ni Store</span>
+                <Link to="/games">
+                    <img
+                        src="https://cdn.discordapp.com/attachments/905481762925338714/924400295214202900/homepage.jpg"
+                        alt="advertisement cover"
+                    />
+                </Link>
+            </div>
+        );
     }
-};
+}
 
 export default HomePage;
